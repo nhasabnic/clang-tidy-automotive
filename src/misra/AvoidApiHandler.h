cf505eadcae30996bb54c84bdc217771c3e0286f
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-02-04 06:45:20 +0100

    More work on the avoid API functionality.

---
+++ src/misra/AvoidApiHandler.h
@@ -0,0 +1,44 @@
+//===--- AvoidApiHandler.h - clang-tidy -------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISRA_AVOIDAPIHANDLER_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISRA_AVOIDAPIHANDLER_H
+
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Lex/PPCallbacks.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace clang::tidy::misra {
+
+class MatchHeaderInfo {
+public:
+  const clang::SourceLocation Location;
+  const Token &Tok;
+  const StringRef Name;
+  const bool IsAngled;
+};
+
+class MatchMacroInfo {
+public:
+  const MatchHeaderInfo *Header;
+  const Token &Tok;
+  const StringRef Name;
+};
+
+class AvoidApiHandler {
+public:
+  virtual ~AvoidApiHandler() = default;
+
+  virtual void checkHeader(const MatchHeaderInfo &Result) {}
+
+  virtual void checkMacro(const MatchMacroInfo &Result) {}
+};
+
+} // namespace clang::tidy::misra
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISRA_AVOIDAPIHANDLER_H
--- src/misra/AvoidApiPPCallbacks.h
+++ src/misra/AvoidApiPPCallbacks.h
@@ -9,7 +9,7 @@
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISRA_AVOIDAPIPPCALLBACKS_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_MISRA_AVOIDAPIPPCALLBACKS_H
 
-#include "AbstractCheckApiHandler.h"
+//#include "AvoidApiHandler.h"
 #include "clang/Lex/PPCallbacks.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
@@ -18,13 +18,7 @@ namespace clang::tidy::misra {
 
 class AvoidApiPPCallbacks : public PPCallbacks {
 public:
-  // Konstruktor som tar emot en AbstractAvoidApiHandler, en ArrayRef och en
-  // valfri header-fil
-  AvoidApiPPCallbacks(AbstractCheckApiHandler &Handler,
-                      llvm::ArrayRef<llvm::StringRef> ForbiddenMacros,
-                      llvm::StringRef HeaderFile = "")
-      : Handler(Handler), ForbiddenMacros(ForbiddenMacros),
-        HeaderFile(HeaderFile) {}
+  AvoidApiPPCallbacks() {}
 
   void InclusionDirective(SourceLocation DirectiveLoc,
                           const Token &IncludeToken, StringRef IncludedFilename,
@@ -37,12 +31,6 @@ public:
   void MacroExpands(const Token &MacroNameTok, const MacroDefinition &MD,
                     SourceRange Range, const MacroArgs *Args) override;
 
-private:
-  AbstractCheckApiHandler &Handler; // Referens till hanteraren
-  llvm::ArrayRef<llvm::StringRef>
-      ForbiddenMacros;        // Referens till förbjudna makron
-  llvm::StringRef HeaderFile; // Header-fil (kan vara tom)
-  std::unique_ptr<MatchHeaderInfo> Header = nullptr;
 };
 
 } // namespace clang::tidy::misra
