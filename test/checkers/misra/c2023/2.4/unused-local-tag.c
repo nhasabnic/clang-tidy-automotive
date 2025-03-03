4baf0765fc78fdc4561b96aa657eed860980a454
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-03-03 22:55:26 +0100

    Updated test case for rule 2.4

---
+++ test/checkers/misra/c2023/2.4/unused-local-tag.c
@@ -0,0 +1,24 @@
+// RUN: %check_clang_tidy %s misra-c2023-adv-2.4 %t -- -- -std=c90
+// RUN: %check_clang_tidy %s misra-c2023-adv-2.4 %t -- -- -std=c99
+// RUN: %check_clang_tidy %s misra-c2023-adv-2.4 %t -- -- -std=c11
+
+
+void f_enum(void) {
+  enum color { RED, GREEN, BLUE }; /* Not compliant */
+// CHECK-MESSAGES: :[[@LINE-1]]:8: warning: unused tag 'color' [misra-c2023-adv-2.4]
+
+  enum status { OK, FAIL };        /* Compliant - Used below. */
+
+  enum status test = OK;
+
+  typedef enum ready {             /* Not compliant */
+// CHECK-MESSAGES: :[[@LINE-1]]:8: warning: unused tag 'ready' [misra-c2023-adv-2.4]  
+    INIT, 
+    READY, 
+    WAIT 
+  } ready_t;
+
+  ready_t r = INIT;
+}
+
+
