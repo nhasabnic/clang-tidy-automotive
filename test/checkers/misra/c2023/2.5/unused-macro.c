a293d084164f9e47fd8826d946b3f4c0dff68315
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-03-03 22:22:27 +0100

    cleanup of test for rule 2.5

--- test/checkers/misra/c2023/2.5/unused-macro.c
+++ test/checkers/misra/c2023/2.5/unused-macro.c
@@ -1,7 +1,9 @@
-// RUN: %check_clang_tidy %s misra-c2023-adv-2.5 %t
+// RUN: %check_clang_tidy %s misra-c2023-adv-2.5 %t -- -- -std=c90
+// RUN: %check_clang_tidy %s misra-c2023-adv-2.5 %t -- -- -std=c99
+// RUN: %check_clang_tidy %s misra-c2023-adv-2.5 %t -- -- -std=c11
 
 #define SIZE 4  /* Compliant */
-#define DATA 3  /* Non compliant */
+#define DATA 3  /* Not compliant */
 // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: Unused macro definition 'DATA' [misra-c2023-adv-2.5]
 
 int a[SIZE];
@@ -9,5 +11,5 @@ int a[SIZE];
 #define TEMP 10  /* Compliant - This is due to the undef */
 #undef TEMP       
 
-#define TEST 42  /* Non compliant - Not used after redefined. */  
+#define TEST 42  /* Not compliant - Not used after redefined. */  
 // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: Unused macro definition 'TEST' [misra-c2023-adv-2.5]
