015204d17b0022c120f6d1b7af3fc6a7db5cc5cb
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-03-10 22:19:35 +0100

    Added test for rule 2.5

---
+++ test/checkers/misra/c2012/2.5/unused-macro.c
@@ -0,0 +1,14 @@
+// RUN: %check_clang_tidy %s misra-c2012-adv-2.5 %t -- -- -std=c90
+// RUN: %check_clang_tidy %s misra-c2012-adv-2.5 %t -- -- -std=c99
+
+#define SIZE 4  /* Compliant */
+#define DATA 3  /* Not compliant */
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: Unused macro definition 'DATA' [misra-c2012-adv-2.5]
+
+int a[SIZE];
+
+#define TEMP 10  /* Compliant - This is due to the undef */
+#undef TEMP       
+
+#define TEST 42  /* Not compliant - Not used after redefined. */  
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: Unused macro definition 'TEST' [misra-c2012-adv-2.5]
