b70552bb71e195e4c4e09117eed43493c40d399e
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-04-10 08:52:39 +0200

    Updated test for rule 3.2

---
+++ test/checkers/misra/c2012/3.2/avoid-line-splicing-within-comment.c
@@ -0,0 +1,25 @@
+// RUN: %check_clang_tidy %s misra-c2012-req-3.2 %t -- -- -std=c99
+
+// A comment              Compliant
+
+// Another comment \
+   with a line splice     Not compliant
+// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: avoid line-spliceing within a '//' comment [misra-c2012-req-3.2]
+
+// Another comment \
+   with two \
+   line splices           Not compliant
+// CHECK-MESSAGES: :[[@LINE-3]]:20: warning: avoid line-spliceing within a '//' comment [misra-c2012-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-3]]:13: warning: avoid line-spliceing within a '//' comment [misra-c2012-req-3.2]
+
+/\
+/\
+/\
+printf("chrismas tree");  Not compliant
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+
+/* \
+                          Compliant - block comment '/*' allows line splice
+*/
