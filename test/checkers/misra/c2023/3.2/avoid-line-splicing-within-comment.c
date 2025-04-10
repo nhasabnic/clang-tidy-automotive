50749c482bed85b7315b11e8c2477e4eed3e478e
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-04-10 08:46:13 +0200

    Updated test for rule 3.2 with a special case.

--- test/checkers/misra/c2023/3.2/avoid-line-splicing-within-comment.c
+++ test/checkers/misra/c2023/3.2/avoid-line-splicing-within-comment.c
@@ -5,10 +5,22 @@
 
 // Another comment \
    with a line splice     Not compliant
-// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: avoid line-spliceing within a // comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
 
 // Another comment \
    with two \
    line splices           Not compliant
-// CHECK-MESSAGES: :[[@LINE-3]]:20: warning: avoid line-spliceing within a // comment [misra-c2023-req-3.2]
-// CHECK-MESSAGES: :[[@LINE-3]]:13: warning: avoid line-spliceing within a // comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-3]]:20: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-3]]:13: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+
+/\
+/\
+/\
+printf("chrismas tree");  Not compliant 
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [misra-c2023-req-3.2]
+
+/* \
+                          Compliant - block comment '/*' allows line splice
+*/
