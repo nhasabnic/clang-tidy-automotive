6bdb3287f84ba08fe2f5fb91dd97e8fb37e2cd60
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-05-16 22:49:00 +0200

    Cleanup.

--- src/misra/goto-stmt/AvoidGotoCheck.cpp
+++ src/misra/goto-stmt/AvoidGotoCheck.cpp
@@ -21,8 +21,7 @@ void AvoidGotoCheck::registerMatchers(MatchFinder *Finder) {
 
 void AvoidGotoCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedDecl = Result.Nodes.getNodeAs<GotoStmt>("goto");
-  diag(MatchedDecl->getGotoLoc(), "avoid goto statement",
-       DiagnosticIDs::Warning);
+  diag(MatchedDecl->getGotoLoc(), "avoid goto statement");
 }
 
 } // namespace clang::tidy::misra
