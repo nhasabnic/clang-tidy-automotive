b2947756786364b73bdb596ab3df67e70b2b6fcb
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-05-22 22:06:54 +0200

    Added a class that can be used in a way so a MISRA check can have a different output compared to when it is executed normally.

---
+++ src/automotive/AutomotiveTidyCheck.h
@@ -0,0 +1,28 @@
+//===--- AutomotiveTidyCheck.h - clang-tidy ---------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOMOTIVE_AUTOMOTIVETIDYCHECK_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOMOTIVE_AUTOMOTIVETIDYCHECK_H
+
+#include "../ClangTidyCheck.h"
+
+namespace clang::tidy::automotive {
+
+class AutomotiveTidyCheck : public ClangTidyCheck {
+public:
+  AutomotiveTidyCheck(StringRef Name, ClangTidyContext *Context)
+      : ClangTidyCheck(Name, Context) {}
+
+  virtual void registerMatchers(ast_matchers::MatchFinder *Finder) override {}
+  virtual void check(const ast_matchers::MatchFinder::MatchResult &Result) override {}
+
+};
+
+} // namespace clang::tidy::automotive
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOMOTIVE_AUTOMOTIVETIDYCHECK_H
