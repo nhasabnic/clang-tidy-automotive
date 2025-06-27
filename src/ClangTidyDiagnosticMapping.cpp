e5c7caa2bd11cc88ffbb73ca2983d2673e3566a7
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-06-27 23:00:04 +0200

    Started to experiment on a mapping functionality.

---
+++ src/ClangTidyDiagnosticMapping.cpp
@@ -0,0 +1,41 @@
+//===--- tools/extra/clang-tidy/ClangTidyDiagnosticMapping.cpp -----------=== //
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "ClangTidyDiagnosticMapping.h"
+#include "ClangTidy.h"
+
+namespace clang::tidy {
+
+void ClangTidyDiagnosticMapping::clear() {
+  DiagConsumer.clear();
+}
+
+void ClangTidyDiagnosticMapping::BeginSourceFile(const LangOptions &LangOpts, const Preprocessor *PP) {
+  DiagConsumer.BeginSourceFile(LangOpts, PP);
+}
+
+void ClangTidyDiagnosticMapping::EndSourceFile() {
+  DiagConsumer.EndSourceFile();
+}
+
+void ClangTidyDiagnosticMapping::finish() {
+  DiagConsumer.finish();
+}
+
+bool ClangTidyDiagnosticMapping::IncludeInDiagnosticCounts () const {
+  return DiagConsumer.IncludeInDiagnosticCounts();
+}
+
+void ClangTidyDiagnosticMapping::HandleDiagnostic(
+    DiagnosticsEngine::Level DiagLevel, const Diagnostic &Info) {
+
+  DiagConsumer.HandleDiagnostic(DiagLevel, Info);
+  DiagnosticConsumer::HandleDiagnostic(DiagLevel, Info);
+}
+
+} // namespace clang::tidy
---
+++ src/ClangTidyDiagnosticMapping.h
@@ -0,0 +1,36 @@
+//===--- ClangTidyDiagnosticMapping.h - clang-tidy --------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANGTIDYDIAGNOSTICMAPPING_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANGTIDYDIAGNOSTICMAPPING_H
+
+#include "clang/Basic/Diagnostic.h"
+
+namespace clang::tidy {
+
+class ClangTidyDiagnosticMapping : public DiagnosticConsumer {
+public:
+  ClangTidyDiagnosticMapping(DiagnosticConsumer &Consumer) :
+                           Consumer(Consumer) {}
+
+  void clear() override;
+  void BeginSourceFile(const LangOptions &LangOpts, const Preprocessor *PP=nullptr) override;
+  void EndSourceFile() override;
+  void finish() override;
+  bool IncludeInDiagnosticCounts () const override;
+  void HandleDiagnostic(DiagnosticsEngine::Level DiagLevel,
+                        const Diagnostic &Info) override;
+
+private:
+  DiagnosticConsumer &DiagConsumer;
+};
+
+} // namespace clang::tidy
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANGTIDYDIAGNOSTICMAPPING_H
+
